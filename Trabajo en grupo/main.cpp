c0b771579e01c85fdc7e4f42a1a8a92796683754
AlvaNeedsFood <aalvarez.contact05@gmail.com>
2023-10-05 14:12:59 -0500

    Initial Commit

---
+++ Trabajo en grupo/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Trabajo en grupo
+//
+//  Created by Hope Alvarez on 5/10/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
